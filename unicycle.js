b0abec50fe5f93cfab09692d3f77bff37efde2a6
Arlo Watts <67204708+arlowatts@users.noreply.github.com>
2026-03-17 11:25:38 -0700

    Minor improvements

--- unicycle.js
+++ unicycle.js
@@ -1,43 +1,56 @@
 addEventListener("load", main);
 
 // initialize motion constants
-const impulse = 0.25;
-const deceleration = -0.00015;
-const maximumVelocity = 1;
+const impulse = 0.2;
+const deceleration = -0.0002;
+const maximumVelocity = 1.2;
 
 // declare the initial state
-let stopTime = 0;
-let initialTime = 0;
-let initialVelocity = 0;
 let initialPosition = 0;
+let initialVelocity = 0;
+let initialTime = 0;
+let motionDuration = 0;
 
 function main() {
 
-    // access the svg elements in the iframe
-    const frame = document.getElementById("frame").contentDocument;
-    const wheel = frame.getElementById("wheel");
-    const pedal = frame.getElementById("pedal");
+    // access the iframe content
+    const frameDocument = document.body.firstElementChild.contentDocument;
 
-    // disable touch actions on the graphic
-    const svg = frame.firstChild;
-    svg.style["touch-action"] = "none";
+    // reference the top-level elements in the document and in the iframe
+    const html = document.documentElement;
+    const svg = frameDocument.documentElement;
+
+    // access the svg elements in the graphic
+    const wheel = svg.getElementById("wheel");
+    const pedal = svg.getElementById("pedal");
 
     // store the initial svg transformation strings
     const wheelInitialTransform = wheel.getAttribute("transform") || "";
     const pedalInitialTransform = pedal.getAttribute("transform") || "";
 
+    // disable touch actions and text selection
+    html.style["touch-action"] = "none";
+    html.style["user-select"] = "none";
+    html.style["-webkit-user-select"] = "none";
+    svg.style["touch-action"] = "none";
+    svg.style["user-select"] = "none";
+    svg.style["-webkit-user-select"] = "none";
+
     // initialize the event listener for user interaction
-    frame.addEventListener("pointerdown", accelerate);
+    html.addEventListener("pointerdown", accelerate);
+    svg.addEventListener("pointerdown", accelerate);
 
     // begin the animation loop
     requestAnimationFrame(animate);
 
     function animate() {
-        let time = Date.now();
+
+        // compute the current position of the wheel
+        const currentPosition = position(Math.min(Date.now() - initialTime, motionDuration));
 
         // apply a rotation to the wheel and the pedals
-        wheel.setAttribute("transform", wheelInitialTransform + `rotate(${+position(time)})`);
-        pedal.setAttribute("transform", pedalInitialTransform + `rotate(${-position(time)})`);
+        wheel.setAttribute("transform", wheelInitialTransform + `rotate(${+currentPosition})`);
+        pedal.setAttribute("transform", pedalInitialTransform + `rotate(${-currentPosition})`);
 
         // request the next frame of the animation
         requestAnimationFrame(animate);
@@ -45,23 +58,25 @@ function main() {
 }
 
 // return the position of the wheel at a given time
-function position(time) {
-    let elapsedTime = Math.min(time - initialTime, stopTime);
-    return initialPosition + (initialVelocity + deceleration * 0.5 * elapsedTime) * elapsedTime;
+function position(elapsedTime) {
+    return (initialPosition + (initialVelocity + deceleration * elapsedTime * 0.5) * elapsedTime) % 360;
 }
 
 // return the velocity of the wheel at a given time
-function velocity(time) {
-    let elapsedTime = Math.min(time - initialTime, stopTime);
+function velocity(elapsedTime) {
     return initialVelocity + deceleration * elapsedTime;
 }
 
 // accelerate the wheel
 function accelerate() {
-    let time = Date.now();
 
-    initialPosition = position(time);
-    initialVelocity = Math.min(maximumVelocity, velocity(time) + impulse);
-    initialTime = time;
-    stopTime = -initialVelocity / deceleration;
+    // compute the elapsed time since the previous initial state
+    const currentTime = Date.now();
+    const elapsedTime = Math.min(currentTime - initialTime, motionDuration);
+
+    // store the current state as the new initial state and update the velocity
+    initialPosition = position(elapsedTime);
+    initialVelocity = Math.min(maximumVelocity, velocity(elapsedTime) + impulse);
+    initialTime = currentTime;
+    motionDuration = -initialVelocity / deceleration;
 }
